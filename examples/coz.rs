903ca315eecb12aa70c8a1aa11df2bab343dbb2f
LoganDark <git@logandark.mozmail.com>
2022-06-15 18:51:47 -0700

    Ugh just make coz noop without the features

--- examples/coz.rs
+++ examples/coz.rs
@@ -1,17 +1,19 @@
-use imgref::ImgVec;
-use stackblur_iter::blur_srgb;
-
 fn main() {
-	coz::thread_init();
+	#[cfg(all(feature = "coz", feature = "blend-srgb"))] {
+		use imgref::ImgVec;
+		use stackblur_iter::blur_srgb;
 
-	const WIDTH: usize = 640;
-	const HEIGHT: usize = 480;
+		coz::thread_init();
 
-	let mut buffer = ImgVec::new(vec![0u32; WIDTH * HEIGHT], WIDTH, HEIGHT);
+		const WIDTH: usize = 640;
+		const HEIGHT: usize = 480;
 
-	loop {
-		coz::begin!("blur");
-		blur_srgb(&mut buffer.as_mut(), 16);
-		coz::end!("blur");
+		let mut buffer = ImgVec::new(vec![0u32; WIDTH * HEIGHT], WIDTH, HEIGHT);
+
+		loop {
+			coz::begin!("blur");
+			blur_srgb(&mut buffer.as_mut(), 16);
+			coz::end!("blur");
+		}
 	}
 }
